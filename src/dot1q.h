ed8d268ea5e0ae0e5699eb991446664d7fb8d7a6
aoshiken <aoshiken@users.noreply.github.com>
2017-08-28 11:02:23 +0000

    Fix usage of includes guard

--- src/dot1q.h
+++ src/dot1q.h
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#ifndef __DOT_1Q_H
+#ifndef __DOT_1Q_H__
 #define __DOT_1Q_H__
 
 #include <libnet.h>
--- src/mpls.h
+++ src/mpls.h
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#ifndef __MPLS_H
+#ifndef __MPLS_H__
 #define __MPLS_H__
 
 #include <libnet.h>
